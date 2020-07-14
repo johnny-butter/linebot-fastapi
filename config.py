0850a0f70245bc252f75d175ca466a57f9735fb2
johnny-butter <butter3126@gmail.com>
2020-07-15 02:51:21 +0800

    [config] add weather api auth code

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     LINE_CHANNEL_ACCESS_TOKEN: str
     LINE_CHANNEL_SECRET: str
+    WEATHER_OPEN_API_AUTH_CODE: str
 
     class Config:
         env_file = '.env'
